6060176ce42a02603ee1df6afbbe73b37b84ffbd
rahulsubh <102258593+rahulsubh@users.noreply.github.com>
2024-06-14 13:12:06 +0530

    created our project blog-app and added master page concept.

---
+++ blog-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Button } from 'reactstrap';
+import Base from './components/Base';
+
+function App() {
+  return (
+    <Base>
+      <h1>This is App Component</h1>
+    </Base>
+  );
+}
+
+export default App;
---
+++ blog-app/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Base from '../components/Base'
+
+function About() {
+  return (
+    <Base>
+      <h1>This is About page</h1>
+      <p>we are building blog website</p>
+    </Base>
+  )
+}
+
+export default About
